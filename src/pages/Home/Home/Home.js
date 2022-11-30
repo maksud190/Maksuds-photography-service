214248ee49aa0b63bddafecae4bab70ce17ffe5a
maksud190 <maknep3rdoct@gmail.com>
2022-11-30 22:17:07 +0600

    statistic removed from home page

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -21,12 +21,6 @@ const Home = () => {
             <Services></Services>
             <AboutPhotography></AboutPhotography>
             <MyJourney></MyJourney>
-            <div className='flex justify-center my-20'>
-                <div className="radial-progress w-52 h-52 mr-10" style={{ "--value": 20 }}>work refusd: 20%</div>
-                <div className="radial-progress w-52 h-52 mr-10" style={{ "--value": 80 }}>Response to client 80%</div>
-                <div className="radial-progress w-52 h-52" style={{ "--value": 100 }}>Work done on time: 100%</div>
-
-            </div>
         </div>
     );
 };
